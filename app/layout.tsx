9945662cfaaf325ec9d590ea24cbbc86b9357159
miguel garcia <mpgarcia2003@gmail.com>
2026-04-18 11:17:04 -0400

    Fix Clarity: move to body, use src prop instead of inline bootstrap

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,14 +33,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Script id="gtm" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`}
         </Script>
-        <Script id="clarity" strategy="afterInteractive">
-          {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","wd2vttf1qj");`}
-        </Script>
         <JsonLd data={[organizationJsonLd(), websiteJsonLd(), localBusinessJsonLd()]} />
       </head>
       <body className="font-body antialiased text-dark bg-white">
         <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} height="0" width="0" style={{ display: "none", visibility: "hidden" }} /></noscript>
         <Script id="datalayer-init" strategy="beforeInteractive">{`window.dataLayer = window.dataLayer || [];`}</Script>
+        <Script id="clarity" strategy="afterInteractive" src="https://www.clarity.ms/tag/wd2vttf1qj" />
         <div className="flex min-h-screen flex-col">
           <Providers>
             <GlobalPhoneTracker />
